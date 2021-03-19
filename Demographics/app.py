4d5e2ce66b91f7fa48f8897c3727cfcf05b54308
Nate Luscomb <78813744+Nate-Luscomb@users.noreply.github.com>
2021-03-19 17:01:57 -0400

    Added "demographics directory and page"
    This heavily relies on yeshi's formatting and I accidently focused on Aleksa's tab, but some of it is usable!

--- Demographics/app.py
+++ Demographics/app.py
@@ -9,14 +9,65 @@ import plotly.graph_objects as go
 
 
 ###STORING DATA
-hollistic_df = pd.read_csv("drugcount-2.csv") #overview 
-table_df = pd.DataFrame(
+hollistic_df = pd.read_csv("TownTotalcrashesFatalsInjuryNoinjury.csv") #overview 
+
+##Top value finder
+top_val = hollistic_df.nlargest(5, ['Crashes'])
+top_town_list = top_val['Town'].values.tolist()
+top_crash_list = top_val['Crashes'].values.tolist()
+
+
+top_crashes_table_df = pd.DataFrame(
     {
-        "Top 5 Drugs": ["Ethanol", "Cannabinoids","Cocaine","THC","Methadone"],
-        "Number of Cases": ["774","354","179","176","137"],
+        "Top 5 Most Crashes by Town": top_town_list,
+        "Crashes": top_crash_list,
     }
 )
-severity_df = pd.read_csv("Severe.csv")
+
+##Bottom value finder
+bottom_val = hollistic_df.nsmallest(5, ['Crashes'])
+bottom_town_list = bottom_val['Town'].values.tolist()
+bottom_crash_list = bottom_val['Crashes'].values.tolist()
+
+
+bottom_crashes_table_df = pd.DataFrame(
+    {
+        "The 5 Least Crashes by Town": bottom_town_list,
+        "Crashes": bottom_crash_list,
+    }
+)
+
+##Severity list builder
+town_list = hollistic_df['Town'].values.tolist()
+Fatals_list = hollistic_df['Fatals'].values.tolist()
+Injury_list = hollistic_df['Injury'].values.tolist()
+Noinjury_list = hollistic_df['No_Injury'].values.tolist()
+
+severity_town_list = []
+severity_severe_list = []
+severity_count_list = []
+
+for i in range(len(town_list)):
+    severity_town_list.append(town_list[i])
+    severity_town_list.append(town_list[i])
+    severity_town_list.append(town_list[i])
+    
+    severity_severe_list.append("Fatal")
+    severity_severe_list.append("Injuries")
+    severity_severe_list.append("No Injury")
+
+    severity_count_list.append(Fatals_list[i])
+    severity_count_list.append(Injury_list[i])
+    severity_count_list.append(Noinjury_list[i])
+    
+
+severity_df = pd.DataFrame(
+    {
+        "Town": severity_town_list,
+        "Severity": severity_severe_list,
+        "Count": severity_count_list,
+    }
+)
 
 #STYLING EXTRAS
 UCONN_LOGO = "https://production.wordpress.uconn.edu/techpark2018/wp-content/uploads/sites/2664/2018/12/uconn-wordmark-single-white.png"
@@ -33,8 +84,8 @@ card_graph = dbc.Card(
         dcc.Graph(id='my_bar', figure={
             'data': [
                     {
-                        "x": hollistic_df["Drug"],
-                        "y": hollistic_df["Count"],
+                        "x": hollistic_df["Town"],
+                        "y": hollistic_df["Crashes"],
                         "type": "bar",
                         "height":00,
                         #'color': colors['text'],
@@ -43,7 +94,7 @@ card_graph = dbc.Card(
                 ],
                 "layout": {
                     "title":{
-                        "text": "Cases Per Drug",
+                        "text": "Crashes per Town",
                         "x": 0.05,
                         "xanchor": "left",
                     },
@@ -58,8 +109,10 @@ card_graph = dbc.Card(
             }),
             body=True, className= 'card border-primary mb-3',
             )
-#Table
-table = dbc.Table.from_dataframe(table_df, striped=True, bordered=True, hover=True, className = 'table-primary thread-dark')
+#Tables
+top_table = dbc.Table.from_dataframe(top_crashes_table_df, striped=True, bordered=True, hover=True, className = 'table-primary thread-dark')
+bottom_table = dbc.Table.from_dataframe(bottom_crashes_table_df, striped=True, bordered=True, hover=True, className = 'table-primary thread-dark')
+
 
 #creating navbar 
 navbar = dbc.Navbar(
@@ -97,32 +150,38 @@ app.layout = dbc.Container([
 
     #title 
     dbc.Row([
-        dbc.Col(html.H2("Toxicology Dashboard",
+        dbc.Col(html.H2("Demographics Dashboard",
                         className='text-center text-primary mb-4'),
                         width = 12)
     ]),
-    ###hollistic bar graph and table 
+    ###hollistic bar graph
     dbc.Row([
-        dbc.Col(card_graph, width=11),
-        dbc.Col(table, width= 1),
+        dbc.Col(card_graph, width=12),
     ]),
 
     #fatal vs severe dropdown and graph
     dbc.Row([
         dbc.Col([
-            html.P("Select Drug:",
+            html.P("Select Town:",
                    style={"textDecoration": "underline"}),
-            dcc.Dropdown(id='my-dpdn', multi=False, value='Ethanol',
+            dcc.Dropdown(id='my-dpdn', multi=False, value='Andover',
                          options=[{'label':x, 'value':x}
-                                  for x in severity_df['Drug'].unique()],
+                                  for x in severity_df['Town'].unique()],
                          ),
             card_graph2,
-        #dcc.Graph(id ='severity-fig', figure={})
+        ##dcc.Graph(id ='severity-fig', figure={})
         ], 
         xs=10, sm=12, md=12, lg=5, xl=5
         ),
-    ])
+        dbc.Col(top_table),
+        dbc.Col(bottom_table),
+    ]),
+    
+    html.Iframe(src="google.com",
+                style={"height": "1067px", "width": "100%"})
 ])
+
+
 
 ###CALLBACKS 
 
@@ -130,13 +189,13 @@ app.layout = dbc.Container([
     Output('severity-fig','figure'),
     [Input('my-dpdn','value')])
 
-def update_bar_chart(drug):
-    mask = severity_df["Drug"] == drug
+def update_bar_chart(Town):
+    mask = severity_df["Town"] == Town
     fig = px.bar(severity_df[mask], x="Severity", y="Count", #color = "Severity",
                  barmode="group")
     fig.update_layout({
         "title":{
-                        "text": "Severity of Cases by Drug",
+                        "text": "Severity of Cases by Town",
                         "x": 0.05,
                         "xanchor": "left",
                     },
@@ -155,4 +214,4 @@ def update_bar_chart(drug):
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port = 5501)
+    app.run_server(debug=True, port = 8050)
