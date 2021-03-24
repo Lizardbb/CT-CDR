7b6f26da3ff1c13ee6c7695c69ee89df7d0f1ec8
yeshi-soleti <78772066+yeshi-soleti@users.noreply.github.com>
2021-03-24 14:05:11 -0400

    Add files via upload

--- Toxicology Data/app.py
+++ Toxicology Data/app.py
@@ -16,8 +16,10 @@ table_df = pd.DataFrame(
         "Number of Cases": ["774","354","179","176","137"],
     }
 )
-severity_df = pd.read_csv("Severe.csv")
-
+severity_df = pd.read_csv("drugbyFatalitysplitAll.csv")
+severity2_df = pd.read_csv("DrugFatalTwoCollim30-4.csv")
+age_df = pd.read_csv("drugByAgeGroupLinear.csv")
+gender_df = pd.read_csv("drugBySex.csv")
 #STYLING EXTRAS
 UCONN_LOGO = "https://production.wordpress.uconn.edu/techpark2018/wp-content/uploads/sites/2664/2018/12/uconn-wordmark-single-white.png"
 
@@ -28,40 +30,55 @@ colors = {
 ###STYLESHEET
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
-### Hollistic Data Card 
-card_graph = dbc.Card(
-        dcc.Graph(id='my_bar', figure={
-            'data': [
-                    {
-                        "x": hollistic_df["Drug"],
-                        "y": hollistic_df["Count"],
-                        "type": "bar",
-                        "height":00,
-                        #'color': colors['text'],
-                        #'fontColor': ["#FFFFFF"],
-                    },
-                ],
-                "layout": {
-                    "title":{
-                        "text": "Cases Per Drug",
-                        "x": 0.05,
-                        "xanchor": "left",
-                    },
-                    'paper_bgcolor':'rgba(0,0,0,0)',
-                    'plot_bgcolor':'rgba(0,0,0,0)',
-                    "colorway": ["#00e3ff"],
-                    'font': {
-                    'color': colors['text'],
-                    'size': 11
-                }
-                },
-            }),
-            body=True, className= 'card border-primary mb-3',
-            )
-#Table
-table = dbc.Table.from_dataframe(table_df, striped=True, bordered=True, hover=True, className = 'table-primary thread-dark')
 
-#creating navbar 
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "20rem",
+    "padding": "2rem 1rem",
+    "background-color": "#2B2D2F",
+
+}
+
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "9.5rem",
+    "margin-right": "2rem",
+    "padding": "0rem 1rem",
+}
+#SIDEBAR 
+sidebar = html.Div(
+    [
+        html.H4("Toxicology Analytics", className="display-4"),
+        html.Hr(),
+        html.P(
+            "Select a Drug Below to Learn More", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.RadioItems(
+                    options=[{'label':x, 'value':x}
+                                  for x in hollistic_df['Drug'].unique()
+            ],
+                    style={ "overflow-y":"scroll", "height": "400px",'display': 'inline-block'},
+                    #labelStyle={'display': 'inline-block'},
+                    value=1,
+                    id="radioitems-input",
+        ),
+            ],
+            vertical=True,
+            pills=True,
+            
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+        
+
 navbar = dbc.Navbar(
     [
         html.A(
@@ -77,58 +94,71 @@ navbar = dbc.Navbar(
             href="https://plot.ly",
         ),
         dbc.NavbarToggler(id="navbar-toggler"),
-        #dbc.Collapse(dbc.Nav, id="navbar-collapse", navbar=True),
     ],
     color="darkblue",
     dark=True,
 )
 
-#severity bar graph - CARD 
-card_graph2 = dbc.Card(
-        dcc.Graph(id='severity-fig', figure={}), body=True, className= 'card border-primary mb-3',
+#content = html.Div(id="page-content", style=CONTENT_STYLE)
+content = dbc.Container(
+    [
+    dbc.Row([
+        dbc.Col(navbar,width = 12, style=CONTENT_STYLE)
+    ]),
+    dbc.Row([
+        dbc.Col(dcc.Graph(id='my_bar', figure={
+             'data': [
+                     {
+                         "x": hollistic_df["Drug"],
+                         "y": hollistic_df["Count"],
+                         "type": "bar",
+                         "height":00,
+                     },
+                 ],
+                 "layout": {
+                     "title":{
+                         "text": "Cases Per Drug",
+                         "x": 0.05,
+                         "xanchor": "left",
+                     },
+                     'paper_bgcolor':'rgba(0,0,0,0)',
+                     'plot_bgcolor':'rgba(0,0,0,0)',
+                     "colorway":["#7DF9FF"],
+                     'font': {
+                     'color': colors['text'],
+                     'size': 12,
+                 }
+                 },
+        }
+        ),
+        style=CONTENT_STYLE,
+        width =12,
+        )
+    ]),
+
+    dbc.Row([
+        dbc.Col(
+            dcc.Graph(id='severity-fig', figure={}), style=CONTENT_STYLE, width=4
+        ),
+        #adding pie
+        dbc.Col(
+            dcc.Graph(id = "age-fig", figure ={}), width=6
+        ),
+        dbc.Col(
+            dcc.Graph(id = "gender-fig", figure ={}), style = CONTENT_STYLE,width=4
+        )
+        
+    ])
+    ],
 )
 
-###LAYOUT 
-app.layout = dbc.Container([
-    #Navbar 
-    dbc.Row([
-        dbc.Col(navbar,width = 12)
-    ]),
 
-    #title 
-    dbc.Row([
-        dbc.Col(html.H2("Toxicology Dashboard",
-                        className='text-center text-primary mb-4'),
-                        width = 12)
-    ]),
-    ###hollistic bar graph and table 
-    dbc.Row([
-        dbc.Col(card_graph, width=11),
-        dbc.Col(table, width= 1),
-    ]),
+app.layout = html.Div([sidebar, content])
 
-    #fatal vs severe dropdown and graph
-    dbc.Row([
-        dbc.Col([
-            html.P("Select Drug:",
-                   style={"textDecoration": "underline"}),
-            dcc.Dropdown(id='my-dpdn', multi=False, value='Ethanol',
-                         options=[{'label':x, 'value':x}
-                                  for x in severity_df['Drug'].unique()],
-                         ),
-            card_graph2,
-        #dcc.Graph(id ='severity-fig', figure={})
-        ], 
-        xs=10, sm=12, md=12, lg=5, xl=5
-        ),
-    ])
-])
-
-###CALLBACKS 
 
 @app.callback(
     Output('severity-fig','figure'),
-    [Input('my-dpdn','value')])
+    [Input('radioitems-input','value')])
 
 def update_bar_chart(drug):
     mask = severity_df["Drug"] == drug
@@ -142,7 +172,7 @@ def update_bar_chart(drug):
                     },
         'plot_bgcolor': 'rgba(0, 0, 0, 0)',
         'paper_bgcolor': 'rgba(0, 0, 0, 0)',
-        'colorway': ["#00e3ff"],
+        'colorway': ["#7DF9FF"],
         'font': {
                     'color': colors['text'],
                     'size': 11
@@ -150,10 +180,70 @@ def update_bar_chart(drug):
         })
     return fig
 
+@app.callback(
+    Output('age-fig','figure'),
+    [Input('radioitems-input','value')])
+
+def update_age_chart(drug):
+    mask = age_df["Drug"] == drug
+    fig = px.bar(age_df[mask], y="Age_Group", x="Count",orientation = 'h', #color = "Severity",
+                 barmode="group")
+    fig.update_layout({
+        "title":{
+                        "text": "Age Group",
+                        "x": 0.05,
+                        "xanchor": "left",
+                    },
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        'colorway': ["#7DF9FF"],
+        'font': {
+                    'color': colors['text'],
+                    'size': 11
+                }
+        })
+    return fig
+    
+@app.callback(
+    Output('gender-fig','figure'),
+    [Input('radioitems-input','value')])
+
+def update_age_chart(drug):
+    mask = gender_df["Drug"] == drug
+    fig = px.bar(gender_df[mask], x="Sex", y="DrugByGender", #color = "Severity",
+                 barmode="group")
+    fig.update_layout({
+        "title":{
+                        "text": "Drug Use by Gender",
+                        "x": 0.05,
+                        "xanchor": "left",
+                    },
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        'colorway': ["#7DF9FF"],
+        'font': {
+                    'color': colors['text'],
+                    'size': 11
+                }
+        })
+    return fig
+    
+
+
+
+
+# @app.callback(
+#     Output('pie-chart','figure'),
+#     [Input('radioitems-input','value')]
+# )
+
+# def update_pie_chart(drug):
+#     mask = age_df["Drug"] == drug
+#     fig = px.pie(age_df[mask], values = ['0-18','19-25','26-34'], names= ['0-18','19-25','26-34'])
+#     return fig
 
 
 
 
 if __name__ == '__main__':
-    app.run_server(debug=True, port = 8050)
-
+    app.run_server(debug=True)
