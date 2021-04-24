785f08f038afbe4880fedbea1af8e1633a1b185d
Nate Luscomb <78813744+Nate-Luscomb@users.noreply.github.com>
2021-04-23 20:49:09 -0400

    Style update to the demographics page

--- Demographics/app.py
+++ Demographics/app.py
@@ -24,18 +24,32 @@ top_crashes_table_df = pd.DataFrame(
     }
 )
 
-##Bottom value finder
-bottom_val = hollistic_df.nsmallest(5, ['Crashes'])
-bottom_town_list = bottom_val['Town'].values.tolist()
-bottom_crash_list = bottom_val['Crashes'].values.tolist()
+#
+#Percent of crashes by pop
 
+#population data
+pop_data = pd.read_csv("2019censusdata.csv")
 
-bottom_crashes_table_df = pd.DataFrame(
+pop_list = pop_data["Population"].values.tolist()
+
+crash_list = hollistic_df["Crashes"].values.tolist()
+
+perc_list = []
+
+for i in range(len(pop_list)):
+    perc_list.append(round((crash_list[i]/pop_list[i]), 2))
+    
+
+perc_list_df = pd.DataFrame(
     {
-        "The 5 Least Crashes by Town": bottom_town_list,
-        "Crashes": bottom_crash_list,
+        "Town": pop_data["Town"].values.tolist(),
+        "Crashes divided by Pop": perc_list,
     }
 )
+
+
+perc_crashes_table_df = perc_list_df.nlargest(5, ['Crashes divided by Pop'])
+
 
 ##Severity list builder
 town_list = hollistic_df['Town'].values.tolist()
@@ -52,8 +66,8 @@ for i in range(len(town_list)):
     severity_town_list.append(town_list[i])
     severity_town_list.append(town_list[i])
     
-    severity_severe_list.append("Fatal")
-    severity_severe_list.append("Injuries")
+    severity_severe_list.append("Fatals")
+    severity_severe_list.append("Injures")
     severity_severe_list.append("No Injury")
 
     severity_count_list.append(Fatals_list[i])
@@ -66,6 +80,54 @@ severity_df = pd.DataFrame(
         "Town": severity_town_list,
         "Severity": severity_severe_list,
         "Count": severity_count_list,
+    }
+)
+
+#age data frame
+
+age_df = pd.read_csv("crashesTownByAges.csv")
+
+zero_eighteen = age_df["0-18"].values.tolist()
+nineteen_twentyfive = age_df["19-25"].values.tolist()
+twentysix_thirtyfour = age_df["26-34"].values.tolist()
+thirtyfive_fiftyfour = age_df["35-54"].values.tolist()
+fiftyfive_sixtyfour = age_df["55-64"].values.tolist()
+sixtyfive = age_df["65+"].values.tolist()
+
+
+age_town_list = []
+age_range_list = []
+age_count_list = []
+
+for i in range(len(town_list)-1):
+    age_town_list.append(town_list[i])
+    age_town_list.append(town_list[i])
+    age_town_list.append(town_list[i])
+    age_town_list.append(town_list[i])
+    age_town_list.append(town_list[i])
+    age_town_list.append(town_list[i])
+
+    age_range_list.append("0-18")
+    age_range_list.append("19-25")
+    age_range_list.append("26-34")
+    age_range_list.append("35-54")
+    age_range_list.append("55-64")
+    age_range_list.append("65+")
+
+    age_count_list.append(zero_eighteen[i])
+    age_count_list.append(nineteen_twentyfive[i])
+    age_count_list.append(twentysix_thirtyfour[i])
+    age_count_list.append(thirtyfive_fiftyfour[i])
+    age_count_list.append(fiftyfive_sixtyfour[i])
+    age_count_list.append(sixtyfive[i])
+
+
+
+age_df = pd.DataFrame(
+    {
+        "Town": age_town_list,
+        "Range": age_range_list,
+        "Count": age_count_list,
     }
 )
 
@@ -113,8 +175,8 @@ card_graph = dbc.Card(
             body=True, className= 'card border-primary mb-3',
             )
 #Tables
-top_table = dbc.Table.from_dataframe(top_crashes_table_df, striped=True, bordered=True, hover=True, className = 'table-primary thread-dark')
-bottom_table = dbc.Table.from_dataframe(bottom_crashes_table_df, striped=True, bordered=True, hover=True, className = 'table-primary thread-dark')
+total_table = dbc.Table.from_dataframe(top_crashes_table_df, striped=True, bordered=True, hover=True, className = 'table-primary thread-dark')
+perc_table = dbc.Table.from_dataframe(perc_crashes_table_df, striped=True, bordered=True, hover=True, className = 'table-primary thread-dark')
 
 
 #creating navbar 
@@ -144,6 +206,11 @@ card_graph2 = dbc.Card(
         dcc.Graph(id='severity-fig', figure={}), body=True, className= 'card border-primary mb-3',
 )
 
+#Age group bar graph - CARD
+card_graph3 = dbc.Card(
+        dcc.Graph(id='age-fig', figure={}),body=True, className= 'card border-primary mb-3',
+)
+
 ###LAYOUT 
 app.layout = dbc.Container([
     #Navbar 
@@ -164,39 +231,42 @@ app.layout = dbc.Container([
 
     #fatal vs severe dropdown and graph
     dbc.Row([
-        dbc.Col([
-            html.P("Select Town:",
+        html.P("Select Town:  ",
                    style={"textDecoration": "underline"}),
-            dcc.Dropdown(id='my-dpdn', multi=False, value='Andover',
+            dcc.Dropdown(id='my-dpdn', multi=False, value='Andover', style={'height': '30px', 'width': "50%"},
                          options=[{'label':x, 'value':x}
                                   for x in severity_df['Town'].unique()],
-                         ),
-            card_graph2,
-        ##dcc.Graph(id ='severity-fig', figure={})
-        ], 
-        xs=10, sm=12, md=12, lg=5, xl=5
-        ),
-        dbc.Col(top_table),
-        dbc.Col(bottom_table),
+                         )
     ]),
+
+    dbc.Row([
+        dbc.Col(card_graph2),
+        dbc.Col(card_graph3),
+    ]),
+
+    dbc.Row([
+        dbc.Col(total_table),
+        dbc.Col(perc_table),
+    ])
     
-    html.Iframe(src=app.get_asset_url('ctmap.html'),
-                style={"height": "1067px", "width": "100%"})
+    ##html.Iframe(src=app.get_asset_url('ctmap.html'),
+    ##            style={"height": "1067px", "width": "100%"})
 ])
 
 
 
 ###CALLBACKS 
 
-@app.callback(
+@app.callback([
     Output('severity-fig','figure'),
+    Output('age-fig', 'figure')],
     [Input('my-dpdn','value')])
 
 def update_bar_chart(Town):
-    mask = severity_df["Town"] == Town
-    fig = px.bar(severity_df[mask], x="Severity", y="Count", #color = "Severity",
+    mask1 = severity_df["Town"] == Town
+    fig1 = px.bar(severity_df[mask1], x="Severity", y="Count", #color = "Severity",
                  barmode="group")
-    fig.update_layout({
+    fig1.update_layout({
         "title":{
                         "text": "Severity of Cases by Town",
                         "x": 0.05,
@@ -210,9 +280,25 @@ def update_bar_chart(Town):
                     'size': 11
                 }
         })
-    return fig
 
-
+    mask2 = age_df["Town"] == Town
+    fig2 = px.bar(age_df[mask2], x="Range", y="Count", #color = "Severity",
+                 barmode="group")
+    fig2.update_layout({
+        "title":{
+                        "text": "Age Range of Cases by Town",
+                        "x": 0.05,
+                        "xanchor": "left",
+                    },
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        'colorway': ["#00e3ff"],
+        'font': {
+                    'color': colors['text'],
+                    'size': 11
+                }
+        })
+    return fig1, fig2
 
 
 
